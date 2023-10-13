05a40fbb1682f255f7918d28e36320a34a12314f
RisDesu <phanhuy30012003@gmail.com>
2023-10-13 14:00:17 +0700

    Commit 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+import controller.DictionaryManager;
+import view.Menu;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+    public static void main(String[] args) {
+        String title = "Dictionary program";
+        String[] s = new String[]{"Add Word", "Delete Word", "Translate", "Exit"};
+        Menu<String> menu = new DictionaryManager(title, s);
+        menu.run();  
+    }
+}
---
+++ src/main/java/common/Validation.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package common;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class Validation {
+     private final static Scanner in = new Scanner(System.in);
+
+    public static int checkInputIntLimit(int min, int max) {
+        while (true) {
+            try {
+                int result = Integer.parseInt(in.nextLine().trim());
+                if (result < min || result > max) {
+                    throw new NumberFormatException();
+
+                }
+                return result;
+            } catch (NumberFormatException e) {
+                System.err.println("Please input number in rage [" + min + ", " + max + "]");
+                System.out.print("Enter again: ");
+            }
+        }
+    }
+
+    public static String checkInputString() {
+        while (true) {
+            String result = in.nextLine().trim();
+            if (result.isEmpty()) {
+                System.err.println("Not empty");
+                System.out.print("Enter again: ");
+            } else {
+                return result;
+            }
+        }
+    }
+
+    public static boolean checkInputYN() {
+        System.out.print("Do you want to replace(Y/N)? ");
+        while (true) {
+            String result = checkInputString();
+            if (result.equalsIgnoreCase("Y")) {
+                return true;
+            }
+            if (result.equalsIgnoreCase("N")) {
+                return false;
+            }
+            System.err.println("Please input y/Y or n/N.");
+            System.out.print("Enter again: ");
+        }
+    }
+}
